6828fd0d4ea1a57fbaf61ad877cb84f0217a1cf9
Benjamin <ben@congdon.dev>
2022-02-20 08:46:08 -0800

    Allow by-book selection

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,28 @@
-import React, { FormEvent } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {parseKindleSnippets, Snippet} from "./parser/parser";
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import { styled } from '@mui/material/styles';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { parseKindleSnippets, Snippet } from "./parser/parser";
+import Box from "@mui/material/Box";
+import { styled } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+import UploadIcon from "@mui/icons-material/Upload";
+import Snackbar from "@mui/material/Snackbar";
+import TextField from "@mui/material/TextField";
+import QuoteCard from "./components/quoteCard";
+import BookGrid from "./components/bookGrid";
+import { Typography } from "@mui/material";
 
 interface State {
-  formValue: string,
-  snippets: Snippet[],
+  formValue: string;
+  snippets: Snippet[];
+  selectedBook?: number;
+  copySnackbarOpen: boolean;
 }
 
 const KINDLE_SNIPPETS_KEY = "kindle_snippets";
 
-const Input = styled('input')({
-  display: 'none',
+const Input = styled("input")({
+  display: "none",
 });
 
 class App extends React.Component<{}, State> {
@@ -28,79 +33,158 @@ class App extends React.Component<{}, State> {
     let snippets: Snippet[] = snippetsRaw ? JSON.parse(snippetsRaw) : [];
 
     this.state = {
-      formValue: "unset",
+      formValue: "",
       snippets,
+      copySnackbarOpen: false,
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.renderSnippets = this.renderSnippets.bind(this);
+    this.titles = this.titles.bind(this);
   }
 
-  handleChange(event: React.FormEvent<HTMLTextAreaElement>) {
-    console.log(event);
-    this.setState({formValue: event.currentTarget.value});
+  handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    this.setState({ formValue: event.currentTarget.value });
   }
 
   handleSubmit(event: React.SyntheticEvent) {
-    // alert('A name was submitted: ' + this.state.value);
     event.preventDefault();
     const parsedSnippets = parseKindleSnippets(this.state.formValue);
-    this.setState({snippets: parsedSnippets});
+    this.setState({ snippets: parsedSnippets });
     localStorage.setItem(KINDLE_SNIPPETS_KEY, JSON.stringify(parsedSnippets));
   }
 
+  copyToClipboard(snippet: Snippet) {
+    navigator.clipboard.writeText(snippet.content);
+    this.setState({ copySnackbarOpen: true });
+  }
+
+  titles() {
+    return Array.from(new Set(this.state.snippets.map((s) => s.title)));
+  }
+
+  clear() {
+    this.setState({
+      selectedBook: undefined,
+      snippets: [],
+    });
+    localStorage.setItem(KINDLE_SNIPPETS_KEY, "");
+  }
+
   renderSnippets() {
-    return this.state.snippets.slice(0).reverse().map(snippet => 
-      <Card sx={{ minWidth: 275, marginTop: 2.5 }} variant="outlined">
-        <CardContent>
-          <Typography sx={{ mb: 1.5 }} color="text.secondary">
-            {snippet.title} ({snippet.author})
-          </Typography>
-          <Typography variant="body2">
-            {snippet.content}
-          </Typography>
-          <Typography sx={{ fontSize: 14, marginTop: 2 }} color="text.secondary" gutterBottom>
-            Page {snippet.page}
-          </Typography>
-        </CardContent>
-      </Card>
-    );
+    const titles = this.titles();
+    return this.state.snippets
+      .filter(
+        (snippet) =>
+          this.state.selectedBook &&
+          titles[this.state.selectedBook] == snippet.title
+      )
+      .sort((a, b) => {
+        if (a.title !== b.title) {
+          return a.title.localeCompare(b.title);
+        }
+        return a.page - b.page;
+      })
+      .map((snippet, idx) => (
+        <QuoteCard
+          snippet={snippet}
+          onClickCopy={() => this.copyToClipboard(snippet)}
+          key={idx}
+        />
+      ));
   }
 
   render() {
     return (
       <div className="App">
-        <form onSubmit={this.handleSubmit}>
-        <label>
-          Name:
-          <textarea value={this.state.formValue} onChange={this.handleChange} />
-        </label>
-        <br/>
-        <Button variant="contained" component="span">
-          Submit
-        </Button>
-        <label htmlFor="contained-button-file">
-          <Input accept="image/*" multiple type="file" id="contained-button-file" />
-            <Button variant="contained" component="span">
+        <form>
+          <TextField
+            multiline
+            placeholder="Paste Snippets"
+            value={this.state.formValue}
+            onChange={this.handleChange}
+            minRows={3}
+            maxRows={3}
+          />
+          <br />
+          <Button
+            variant="contained"
+            component="span"
+            onClick={() => this.clear()}
+          >
+            Clear
+          </Button>
+          <Button
+            variant="contained"
+            component="span"
+            onClick={this.handleSubmit}
+          >
+            Submit
+          </Button>
+          <label htmlFor="contained-button-file">
+            <Input
+              accept="image/*"
+              multiple
+              type="file"
+              id="contained-button-file"
+            />
+            <Button
+              variant="outlined"
+              component="span"
+              startIcon={<UploadIcon />}
+            >
               Upload
             </Button>
-        </label>
-      </form>
-      <Box sx={{
-        minWidth: 275,
-        maxWidth: 700,
-        textAlign: 'left',
-        alignItems: 'center',
-        marginLeft: 'auto',
-        marginRight: 'auto',
-      }}>
-        {this.renderSnippets()}
-      </Box>
+          </label>
+        </form>
+        <div
+          style={{ display: this.state.snippets.length ? undefined : "none" }}
+        >
+          <Typography variant="h3">Books</Typography>
+          <Box
+            sx={{
+              minWidth: 275,
+              maxWidth: 1200,
+              textAlign: "left",
+              alignItems: "center",
+              marginLeft: "auto",
+              marginRight: "auto",
+              marginTop: 10,
+            }}
+          >
+            <BookGrid
+              titles={this.titles()}
+              selectedBook={this.state.selectedBook}
+              onBookClicked={(idx) => this.setState({ selectedBook: idx })}
+            />
+          </Box>
+          <Typography variant="h3">Snippets</Typography>
+          <Box
+            sx={{
+              minWidth: 275,
+              maxWidth: 700,
+              textAlign: "left",
+              alignItems: "center",
+              marginLeft: "auto",
+              marginRight: "auto",
+              marginTop: 10,
+            }}
+          >
+            {this.renderSnippets()}
+          </Box>
+          <Snackbar
+            open={this.state.copySnackbarOpen}
+            autoHideDuration={3000}
+            onClose={() => {
+              this.setState({ copySnackbarOpen: false });
+            }}
+            message="Snippet copied"
+          />
+        </div>
       </div>
     );
   }
 }
-
 
 export default App;
---
+++ src/components/quoteCard.tsx
@@ -0,0 +1,45 @@
+import { Snippet } from "../parser/parser";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+
+type QuoteCardProps = {
+  snippet: Snippet;
+  onClickCopy: () => void;
+};
+
+const QuoteCard = ({ snippet, onClickCopy }: QuoteCardProps) => (
+  <Card sx={{ minWidth: 275, marginTop: 2.5 }} variant="outlined">
+    <CardHeader
+      disableTypography
+      title={
+        <>
+          <Typography color="text.primary">{snippet.title}</Typography>
+        </>
+      }
+      subheader={
+        <>
+          <Typography color="text.secondary">{snippet.author}</Typography>
+        </>
+      }
+    ></CardHeader>
+    <CardContent>
+      <Typography variant="body2">{snippet.content}</Typography>
+    </CardContent>
+    <CardActions sx={{ justifyContent: "space-between" }} onClick={onClickCopy}>
+      <Typography sx={{ fontSize: 14, marginLeft: 1 }} color="text.secondary">
+        {snippet.page ? `Page ${snippet.page}` : `Location ${snippet.location}`}
+      </Typography>
+      <div className={"flex-grow"} />
+      <IconButton sx={{ alignItems: "0" }}>
+        <ContentCopyIcon />
+      </IconButton>
+    </CardActions>
+  </Card>
+);
+
+export default QuoteCard;
